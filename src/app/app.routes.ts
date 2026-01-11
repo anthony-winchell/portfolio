93652de46c1baa3f7203107b08042042299f9a1a
anthony-winchell <123203535+anthony-winchell@users.noreply.github.com>
2026-01-11 16:35:11 -0500

    generated components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Projects } from './components/projects/projects';
+import { Resume } from './components/resume/resume';
+import { Contact } from './components/contact/contact';
+import { Layout } from './components/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Layout,
+        children: [
+            { path: '', component: Home },
+            { path: 'projects', component: Projects },
+            { path: 'resume', component: Resume },
+            { path: 'contact', component: Contact },
+            { path: '**', redirectTo: '' },
+        ]
+    }
+    
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
+import { Resume } from './components/resume/resume';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Resume, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
